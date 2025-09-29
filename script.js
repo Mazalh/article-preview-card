a3888c140f8acb593b3c92779ee0c62f3240f087
Mazalh <135105639+Mazalh@users.noreply.github.com>
2025-09-29 18:26:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleSharePanel() {
   panel.style.display = panel.style.display === 'flex' ? 'none' : 'flex';
 }
 
-// Клік на іконки (тільки для демонстрації)
+// Іконки Share
 document.querySelector('.facebooklogo').addEventListener('click', () => alert('Facebook'));
 document.querySelector('.instagramlogo').addEventListener('click', () => alert('Instagram'));
 document.querySelector('.copyurl').addEventListener('click', () => alert('Скопійовано!'));
